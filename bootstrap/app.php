ababcc0b793470060bbe9e5766eddbaa6889a9bf
felac <cnastares@gmail.com>
2025-06-23 13:59:09 -0500

    Fix routing namespace duplication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,19 +7,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
-	->withRouting(
-		using: function () {
-			// api
-			Route::middleware('api')
-				->namespace('App\Http\Controllers\Api')
-				->prefix('api')
-				->group(base_path('routes/api.php'));
-			
-			// web
-			Route::middleware('web')
-				->namespace('App\Http\Controllers\Web')
-				->group(base_path('routes/web.php'));
-		},
+        ->withRouting(
+                using: function () {
+                        // api
+                        Route::middleware('api')
+                                ->prefix('api')
+                                ->group(base_path('routes/api.php'));
+
+                        // web
+                        Route::middleware('web')
+                                ->group(base_path('routes/web.php'));
+                },
 		commands: __DIR__ . '/../routes/console.php',
 		// health: '/up',
 	)
